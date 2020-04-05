f968e8177ee2d7fd62f39743b46d0497927d9e28
Katrin00 <rfnhbyf2000@mail.ru>
2020-04-05 18:01:28 +0300

    change fields

--- app/src/main/java/com/example/ada/MainActivity.java
+++ app/src/main/java/com/example/ada/MainActivity.java
@@ -22,49 +22,43 @@ public class MainActivity extends AppCompatActivity {
     private Button b;
     int i=1;
 
-public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
 
-    per=findViewById ( R.id.rv );
-    toWhom = findViewById ( R.id.toWhom );
-    whoApprove = findViewById ( R.id.whoApprove );
+        per=findViewById ( R.id.rv );
+        toWhom = findViewById ( R.id.toWhom );
+        whoApprove = findViewById ( R.id.whoApprove );
 
-    LinearLayoutManager layoutManager = new LinearLayoutManager ( this );
-    LinearLayoutManager layoutManager1 = new LinearLayoutManager ( this );
-    LinearLayoutManager layoutManager2 = new LinearLayoutManager ( this );
+        LinearLayoutManager layoutManager = new LinearLayoutManager ( this );
+        LinearLayoutManager layoutManager1 = new LinearLayoutManager ( this );
+        LinearLayoutManager layoutManager2 = new LinearLayoutManager ( this );
 
-    per.setLayoutManager ( layoutManager );
-    toWhom.setLayoutManager ( layoutManager1 );
-    whoApprove.setLayoutManager ( layoutManager2 );
+        per.setLayoutManager ( layoutManager );
+        toWhom.setLayoutManager ( layoutManager1 );
+        whoApprove.setLayoutManager ( layoutManager2 );
 //
-   toWhomAdapter=new toWhomAdapter ( i );
-    adapter= new Adapter ( i );
-    whoApproveAdapter= new WhoApproveAdapter ( i );
+        toWhomAdapter=new toWhomAdapter ( i );
+        adapter= new Adapter ( i );
+        whoApproveAdapter= new WhoApproveAdapter ( i );
 
-    per.setAdapter ( adapter );
-    toWhom.setAdapter ( toWhomAdapter );
-    whoApprove.setAdapter ( whoApproveAdapter );
+        per.setAdapter ( adapter );
+        toWhom.setAdapter ( toWhomAdapter );
+        whoApprove.setAdapter ( whoApproveAdapter );
 
-    b= findViewById ( R.id.button );
-    b.setOnClickListener ( new View.OnClickListener () {
-        @Override
-        public void onClick(View v) {
-            i++;
-            per.getLayoutParams ().height+=500;
-            adapter= new Adapter ( i );
-            per.setAdapter ( adapter );
-
-
-        }
-    } );
+        b= findViewById ( R.id.button );
+        b.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                i++;
+                per.getLayoutParams ().height+=500;
+                adapter= new Adapter ( i );
+                per.setAdapter ( adapter );
 
 
+            }
+        } );
 
-
-
-
-
-}
+    }
 }
